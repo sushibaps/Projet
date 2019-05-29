9c5c93b000d1e3d65778b7abce4c6607c6abca76
sushibaps <matthias.bapseres@gmail.com>
2019-05-29 06:22:44 +0200

    Ajout des fichiers de création d'articles

---
+++ view/frontend/articleCreation.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Création d'article</title>
+</head>
+<body>
+    <h1>Bienvenue dans l'interface de création d'articles</h1>
+    <form action="" method="post">
+        <textarea name="titre" id="" cols="20" rows="1" placeholder="Veuillez entrer le titre de votre article"></textarea>
+        <br>
+        <textarea name="article" id="" cols="30" rows="10" placeholder="Veuillez entrer le contenu de votre article"></textarea>
+        <button type="button">Publier</button>
+    </form>
+
+</body>
+</html>
