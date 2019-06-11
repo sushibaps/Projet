ad97ffd6814d0c5988c992fe95a05d8a6b37264d
sushibaps <48201943+sushibaps@users.noreply.github.com>
2019-06-11 13:30:38 +0200

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo 'Coucou';
-for ($i = 0; $i < 5; $i++)
-    echo $i;
-
-$data = file_get_contents('http://mbapseres.fr/Projet%201/index.html');
-echo $data;
-file_put_contents('pouet.txt', $data);
